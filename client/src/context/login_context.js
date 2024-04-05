9cfd32bed57f82deae0d92992b029b1e0709f02c
PremSuryan <116444785+PremSuryan@users.noreply.github.com>
2024-04-05 14:31:54 +0530

    Update login_context.js

--- client/src/context/login_context.js
+++ client/src/context/login_context.js
@@ -1,3 +1,19 @@
 import React from "react"
+import { useState } from "react"
 
-const LoginContext = React.createContext()
+export const LoginContext = React.createContext();
+
+export cons LoginProvider = () => {
+ const [ userregister, setUserRegister] = useState(false);
+}
+
+return ( 
+
+<LoginContext.Provider value= { userregister, setUserRegister} >
+<LoginPage />
+<Register />
+<LoginContext.Provider>
+
+
+);
+
